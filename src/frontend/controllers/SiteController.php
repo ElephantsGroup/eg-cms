4351ea454ce426c4ddae7e3a0752583eb671be93
j.jaberi <j_jaberi@yhoo.com>
2018-05-16 15:26:52 +0430

    added stat trait to default controller

--- src/frontend/controllers/SiteController.php
+++ src/frontend/controllers/SiteController.php
@@ -4,12 +4,15 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use elephantsGroup\base\EGController;
+use elephantsGroup\stat\traits\StatTrait;
 
 /**
  * Site controller
  */
 class SiteController extends EGController
 {
+	use StatTrait;
+
     public function actionIndex()
     {
 		return $this->render('index');
